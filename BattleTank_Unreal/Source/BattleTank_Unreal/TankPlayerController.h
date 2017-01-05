e6d4e5b6e84fe8c9a8fa732fd1a6dc4021c3f159
YurgenHohmeyer <yHohmeyer@gmail.com>
2017-01-05 17:23:31 -0500

    WORKING: AimAt function is now working correctly by moving terrain down 100cm and using GetHitResultAtScreenPosition instead of line tracing.

--- BattleTank_Unreal/Source/BattleTank_Unreal/TankPlayerController.h
+++ BattleTank_Unreal/Source/BattleTank_Unreal/TankPlayerController.h
@@ -24,18 +24,5 @@ private:
 	// Start the tank moving the barrel so that shot would hit where the crosshair intersects the world
 	void AimTowardsCrosshair();
 
-	bool GetSightRayHitLocation(FVector& HitLocation) const;
-
-	UPROPERTY(EditAnywhere)
-	float CrossHairXLocation = 0.5;
-
-	UPROPERTY(EditAnywhere)
-	float CrossHairYLocation = 0.3333;
-
-	UPROPERTY(EditAnywhere)
-	float LineTraceRange = 1000000;
-
-	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
-
-	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+	bool GetCrosshairTraceHit(FString &Objecthit, FVector &HitLocation);
 };
